3b1b360c73c223a857cc27f7be0b24fba858dce2
kvcastillo <kenzovcastillo@gmail.com>
2026-05-22 17:49:17 +0800

    fixing config

--- backend/index.ts
+++ backend/index.ts
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use("/api/v1/applications", applicationsRouter);
 
 app.get("/", (req, res) => {
-  res.json({ message: "API is alive" });
+  res.json({ message: "API is alive : ", applicationsRouter });
 });
 
 app.listen(PORT, () => {
