6ac2b7f888d3dfeb2a043218617e4e9839553d19
kvcastillo <kenzovcastillo@gmail.com>
2026-06-08 00:12:46 +0800

    minor syntax fix for eslint warning

--- frontend/vite-project/src/contexts/AuthProvider.tsx
+++ frontend/vite-project/src/contexts/AuthProvider.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useState, useEffect, ReactNode } from "react";
+import { createContext, useState, useEffect, type ReactNode } from "react";
 
 type User = {
   id: string;
