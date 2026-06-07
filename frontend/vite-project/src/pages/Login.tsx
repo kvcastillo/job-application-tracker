4a2e2386237d48377e55fac74db64afe07ff6633
kvcastillo <kenzovcastillo@gmail.com>
2026-06-07 17:29:26 +0800

    Modified Route Mismatch for Render

--- frontend/vite-project/src/pages/Login.tsx
+++ frontend/vite-project/src/pages/Login.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BASE_URL = "https://job-application-tracker-wiqx.onrender.com/api/v1";
+const BASE_URL =
+  "https://job-application-tracker-wiqx.onrender.com/api/v1/auth";
 
 export default function Login() {
   const navigate = useNavigate();
