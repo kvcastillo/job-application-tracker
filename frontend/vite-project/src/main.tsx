067a597181b69378eafa75ebe0a96c761c876ffa
kvcastillo <kenzovcastillo@gmail.com>
2026-06-07 22:54:14 +0800

    added routes

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./src/routes";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navbar />
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>,
